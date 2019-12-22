80ea1f8a950ba200ea84250a5fa93a0ebd7fbb18
Ashish Ranjan <ashishranjan738@gmail.com>
2019-12-23 00:31:30 +0530

    refactor(interviewbit): Refactor code with c++ standards

--- c++/interviewbit/Hashing/Colorful Number.cpp
+++ c++/interviewbit/Hashing/Colorful Number.cpp
@@ -13,8 +13,8 @@ public:
 
 // COLORFUL number:
 
-// A number can be broken into different contiguous sub-subsequence parts. 
-// Suppose, a number 3245 can be broken into parts like 3 2 4 5 32 24 45 324 245. 
+// A number can be broken into different contiguous sub-subsequence parts.
+// Suppose, a number 3245 can be broken into parts like 3 2 4 5 32 24 45 324 245.
 // And this number is a COLORFUL number, since product of every digit of a contiguous subsequence is different
 // Example:
 
@@ -23,48 +23,57 @@ public:
 // 2 -> 2
 // 3 -> 3
 // 23 -> 6
-// this number is a COLORFUL number since product of every digit of a sub-sequence are different. 
+// this number is a COLORFUL number since product of every digit of a sub-sequence are different.
 
 // Output : 1
 
-vector<int> toVector(int a){
+vector<int> toVector(int a)
+{
     vector<int> ans;
-    while(a!=0){
-        ans.push_back(a%10);
-        a/=10;
+    while (a != 0)
+    {
+        ans.push_back(a % 10);
+        a /= 10;
     }
-    return vector<int> (ans.rbegin(),ans.rend());
+    return vector<int>(ans.rbegin(), ans.rend());
 }
 
-int toInt(vector<int> a){
-    int ans=0;
-    for(int i:a){
-        ans*=10;
-        ans+=i;
-    }
-    return ans;
-}
-
-int product(vector<int> a){
-    int ans=1;
-    for(int i:a){
-        ans*=i;
+int toInt(vector<int> a)
+{
+    int ans = 0;
+    for (int i : a)
+    {
+        ans *= 10;
+        ans += i;
     }
     return ans;
 }
 
-int Solution::colorful(int A) {
-    unordered_map<int,bool> m;
-    vector<int> v=toVector(A);
-    for(int l=1;l<=v.size();l++){
-        for(int i=0;i<=v.size()-l;i++){
-            vector<int> temp(v.begin()+i,v.begin()+i+l);
-            
-            int tempInt=toInt(temp);
-            int tempProduct=product(temp);
-            if(m.find(tempInt)!=m.end() || m.find(tempProduct)!=m.end())
+int product(vector<int> a)
+{
+    int ans = 1;
+    for (int i : a)
+    {
+        ans *= i;
+    }
+    return ans;
+}
+
+int Solution::colorful(int A)
+{
+    unordered_map<int, bool> m;
+    vector<int> v = toVector(A);
+    for (int l = 1; l <= v.size(); l++)
+    {
+        for (int i = 0; i <= v.size() - l; i++)
+        {
+            vector<int> temp(v.begin() + i, v.begin() + i + l);
+
+            int tempInt = toInt(temp);
+            int tempProduct = product(temp);
+            if (m.find(tempInt) != m.end() || m.find(tempProduct) != m.end())
                 return 0;
-            m[tempProduct]=true;
+            m[tempProduct] = true;
         }
     }
     return 1;
--- c++/interviewbit/Hashing/Largest Continuous Sequence Zero Sum.cpp
+++ c++/interviewbit/Hashing/Largest Continuous Sequence Zero Sum.cpp
@@ -11,26 +11,29 @@ public:
 
 // Example:
 
-
 // Input:  {1 ,2 ,-2 ,4 ,-4}
 // Output: {2 ,-2 ,4 ,-4}
 
-//  NOTE : If there are multiple correct answers, return the sequence which occurs first in the array. 
+//  NOTE : If there are multiple correct answers, return the sequence which occurs first in the array.
 
-vector<int> Solution::lszero(vector<int> &A) {
-    unordered_map<int,int> m;
-    int sum=0;
+vector<int> Solution::lszero(vector<int> &A)
+{
+    unordered_map<int, int> m;
+    int sum = 0;
     vector<int> ans;
-    m[0]=0;
-    for(int i=1;i<=A.size();i++){
-        sum+=A[i-1];
-        if(m.find(sum)!=m.end()){
-            vector<int> tans(A.begin()+m[sum],A.begin()+i);
-            if(tans.size()>ans.size())
-                ans=tans;
-            
-        } else {
-            m[sum]=i;
+    m[0] = 0;
+    for (int i = 1; i <= A.size(); i++)
+    {
+        sum += A[i - 1];
+        if (m.find(sum) != m.end())
+        {
+            vector<int> tans(A.begin() + m[sum], A.begin() + i);
+            if (tans.size() > ans.size())
+                ans = tans;
+        }
+        else
+        {
+            m[sum] = i;
         }
     }
     return ans;
