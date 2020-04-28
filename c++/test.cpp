4cac8f6979466f99052bf9f7eba3cb1276c37ee8
Ashish Ranjan <ashishranjan738@gmail.com>
2020-04-28 20:33:46 +0530

    test
    Signed-off-by: Ashish Ranjan <ashishranjan738@gmail.com>

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,5 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+
+int clearRange(int n, int i, int j)
+{
+    int ones = (~0);
+    int a = ones << (j + 1);
+    int b = (1 << i) - 1;
+    int mask = a | b;
+    return n & mask;
+}
+
+int replaceBits(int n, int m, int i, int j)
+{
+    int nCleared = clearRange(n, i, j);
+    return nCleared | (m << i);
+}
+
+int main()
+{
+    cout << replaceBits(1024, 21, 2, 6);
     return 0;
 }
